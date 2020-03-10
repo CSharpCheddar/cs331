303325d80d5dd20cc724d884ccbb4deb3be80b82
CSharpCheddar <muellermartin98@gmail.com>
2020-03-10 18:27:47 -0500

    Finished a4

--- a4/a4.js
+++ a4/a4.js
@@ -23,30 +23,52 @@ var prune = function (ns) {
 };
 
 var hasMoreEvensHelper = function (ns, a) {
-
-    /* to be completed */
-    
+  if (fp.isNull(ns)) {
+    return a;
+  } else if (fp.isList(fp.hd(ns))) {
+    return fp.add(hasMoreEvensHelper(fp.hd(ns), a),
+                  hasMoreEvensHelper(fp.tl(ns), a));
+  } else if (fp.isEq(fp.rem(fp.hd(ns), 2), 0)) {
+    return hasMoreEvensHelper(fp.tl(ns), fp.add(a, 1));
+  } else {
+    return hasMoreEvensHelper(fp.tl(ns), fp.sub(a, 1));
+  }
 };
 
 var hasMoreEvens = function (ns) {
-    return true;   /* here you may ONLY modify the "true" constant */
+  return fp.isGT(hasMoreEvensHelper(ns, 0), 0);
 };
 
 var max = function (ns) {
-
-    /* to be completed */
-    
+  return fp.reduce(fp.max, ns, fp.hd(ns));
 };
 
 var countEvens = function (ns) {
-
-    /* to be completed */
+  return fp.reduce(
+    fp.add,
+    fp.map(function (x) {
+             return fp.rem(fp.add(x, 1), 2);
+           },
+           ns
+    ),
+    0
+  );
 };
 
 var getHeaviest = function (ns) {
-
-    /* to be completed */    
-    
+  return fp.reduce(
+    fp.max,
+    fp.map(function (x) {
+             if (fp.isList(x)) {
+               return fp.reduce(fp.add, x, 0);
+             } else {
+               return x;
+             }
+           },
+           ns
+    ),
+    0
+  );
 };
 
 exports.hasMoreEvens = hasMoreEvens;
